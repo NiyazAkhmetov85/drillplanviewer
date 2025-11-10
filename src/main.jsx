4d8f4c2fbae638849d7f94c742a8df7710b4952c
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-11-10 15:20:58 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'; // Общие стили для всего приложения (если есть)
 
+import 'leaflet/dist/leaflet.css'; // Импорт базовых стилей Leaflet
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
