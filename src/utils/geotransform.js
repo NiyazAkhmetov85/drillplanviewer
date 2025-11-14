7deab992f00b19a7711865a94599e68ce3baa3e8
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-11-14 22:02:33 +0500

    Update geotransform.js

--- src/utils/geotransform.js
+++ src/utils/geotransform.js
@@ -1,20 +1,22 @@
 // src/utils/geotransform.js
+
 import proj4 from 'proj4';
 
 // ----------------------------------------------------------------------
 // 1. ПАРАМЕТРЫ СИСТЕМЫ USLOVWGS
 // ----------------------------------------------------------------------
 
-// Начало системы координат в десятичных градусах
-const LAT_0_DECIMAL = 53.41320278;
-const LON_0_DECIMAL = 69.23860833;
+// Широта центральной точки (оставлена старая, так как отсутствует в конфиге [local_grid])
+const LAT_0_DECIMAL = 53.41320278; 
+// Долгота центрального меридиана (из [local_grid] cmeridian=69)
+const LON_0_DECIMAL = 69; 
 
-// Масштаб (Scale factor)
-const SCALE_FACTOR = 1.000097549103;
+// Масштаб (Scale factor) (из [local_grid] cm_scalef=0.99960)
+const SCALE_FACTOR = 0.99960; 
 
-// Ложный восток / север (False Easting / Northing)
-const FALSE_EASTING = 4458.9140;
-const FALSE_NORTHING = 7317.3475;
+// Ложный восток / север
+const FALSE_EASTING = 500000.0; // из [local_grid] false_east=500000.0
+const FALSE_NORTHING = 7317.3475; // оставлена старая
 
 // ----------------------------------------------------------------------
 // 2. Определение систем координат
@@ -23,7 +25,7 @@ const FALSE_NORTHING = 7317.3475;
 // WGS84 (Географическая система, широта/долгота)
 const WGS84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
 
-// USLOVWGS (Локальная проекция Transverse Mercator)
+// USLOVWGS (Локальная проекция Transverse Mercator с обновленными параметрами)
 const USLOVWGS_DEF = `+proj=tmerc
                       +lat_0=${LAT_0_DECIMAL}
                       +lon_0=${LON_0_DECIMAL}
@@ -46,7 +48,7 @@ export function transformUSLOVtoWGS84(easting, northing) {
     if (isNaN(easting) || isNaN(northing)) return [null, null];
 
     try {
-        // ИСПРАВЛЕНО: Входной порядок для proj4: [Easting, Northing] (X, Y)
+        // Входной порядок для proj4: [Easting, Northing] (X, Y)
         const [longitude, latitude] = proj4(USLOVWGS_DEF, WGS84, [easting, northing]);
         
         // Возвращаем в порядке, принятом Leaflet: [Latitude, Longitude] (Lat, Lng)
